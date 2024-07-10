aa114ed8a77b8cec6642765ba15ec952bbbb44bc
YashwanthK <yashwanthkothakota@gmail.com>
2024-07-10 14:49:50 +0530

    duplicate using sorting

--- DataStructures/Array/containsDuplicate.py
+++ DataStructures/Array/containsDuplicate.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Tc: O(N) Sc: O(N)
     def containsDuplicate(self,nums):
         unique_set = set()
         for num in nums:
@@ -6,14 +7,24 @@ class Solution:
                 return True
             unique_set.add(num)
         return False
+    
+    # Tc: O(NlogN) Sc: O(N)
+    def containsDuplicate_sort(self,nums)->bool:
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
 
 if __name__ == '__main__':
   sol = Solution()
   nums1 = [1, 2, 3, 4]
   print(sol.containsDuplicate(nums1)) # Expected output: False
+  print(sol.containsDuplicate_sort(nums1)) # Expected output: False
 
   nums2 = [1, 2, 3, 1]
   print(sol.containsDuplicate(nums2)) # Expected output: True
+  print(sol.containsDuplicate_sort(nums2)) # Expected output: True
 
   nums3 = []
   print(sol.containsDuplicate(nums3)) # Expected output: False
