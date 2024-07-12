fc6c5671def8feae6742a1d33fe462513b62bf11
YashwanthK <yashwanthkothakota@gmail.com>
2024-07-12 11:40:53 +0530

    implement trie

---
+++ DataStructures/Trie/implement_trie.py
@@ -0,0 +1,39 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+    
+class Solution:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word:str)->None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEnd = True
+    
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.isEnd
+    
+    def startsWith(self, prefix:str)->bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+if __name__ == "__main__":
+    trie = Solution()
+    trie.insert("apple")
+    print(trie.search("apple"))  # True
+    print(trie.search("app"))    # False
+    print(trie.startsWith("app"))# True        
